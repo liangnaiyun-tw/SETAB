db4378053829bf79329753218304b53a218d657f
skychocowhite <simon2000.wang@gmail.com>
2023-05-25 16:55:44 +0800

    [Add] Add tab information test

---
+++ src/layout/RightSidebar/SystemMemoryUsage.js
@@ -0,0 +1,71 @@
+import { useEffect, useState } from "react";
+
+/*global chrome*/
+
+function SystemMemoryUsage() {
+  const [history, setHistory] = useState([]);
+
+  function getMemory(processes) {
+    console.log(Date.now());
+    let tabTasks = Object.keys(processes).map((id) => {
+      if (processes[id].tasks[0].tabId !== undefined) {
+        return processes[id];
+      } else {
+        return undefined;
+      }
+    }).filter((task) => task !== undefined);
+
+    console.log(tabTasks);
+
+    let newHistory = [];
+    tabTasks.forEach((task) => {
+      task.tasks.forEach((subTask) => {
+        chrome.processes.getProcessIdForTab(subTask.tabId)
+          .then((id) => {
+            console.log(subTask.tabId);
+            console.log(id);
+          });
+        newHistory.push({
+          id: task.id,
+          parentPid: task.osProcessId,
+          pid: task.osProcessId,
+          tabId: subTask.tabId,
+          tabName: subTask.title,
+          jsMemoryAllocated: task.jsMemoryAllocated,
+          jsMemoryUsed: task.jsMemoryUsed,
+          privateMemory: task.privateMemory,
+        });
+      });
+    });
+    setHistory(newHistory);
+  }
+
+  useEffect(() => {
+    chrome.processes.onUpdatedWithMemory.addListener(getMemory);
+    return () => {
+      chrome.processes.onUpdatedWithMemory.removeListener(getMemory);
+    };
+  }, []);
+
+  return (
+    <div className="SystemMemoryUsage">
+      {history.map((task) => {
+        return (
+          <div key={task.tabId} className="tabTask">
+            <ul>
+              <li>Id: {task.id}</li>
+              <li>Pid: {task.pid}</li>
+              <li>Tab Id: {task.tabId}</li>
+              <li>Tab Name: {task.tabName}</li>
+              <li>Javascript Memory Allocated: {task.jsMemoryAllocated}</li>
+              <li>Javascript Memory Used: {task.jsMemoryUsed}</li>
+              <li>Private Memory: {task.privateMemory}</li>
+            </ul>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export { SystemMemoryUsage };
--- src/layout/RightSidebar/index.js
+++ src/layout/RightSidebar/index.js
@@ -1,10 +1,11 @@
-
 import React from "react";
+import { SystemMemoryUsage } from "./SystemMemoryUsage";
 
 const RightSidebar = ({ cssRightSidebar, styleRightSidebar }) => {
   return (
     <div className={cssRightSidebar} style={styleRightSidebar}>
       RightSidebar
+      <SystemMemoryUsage></SystemMemoryUsage>
     </div>
   );
 };
