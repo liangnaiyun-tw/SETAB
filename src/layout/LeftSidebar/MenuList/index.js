43e1584f8fe00e9c6c631bccc8ec95d9498e4aae
liang-nai-yun <ny.liang.tw@gmail.com>
2023-05-27 14:09:08 +0800

    left sidebar initial

---
+++ src/layout/LeftSidebar/MenuList/index.js
@@ -0,0 +1,98 @@
+
+
+import * as React from 'react';
+import TreeView from '@mui/lab/TreeView';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import TreeItem from '@mui/lab/TreeItem';
+import { UncontrolledTreeEnvironment, Tree, StaticTreeDataProvider } from 'react-complex-tree';
+import 'react-complex-tree/lib/style-modern.css';
+
+export default function MenuList() {
+
+    const items = {
+        root: {
+            index: 'root',
+            canMove: true,
+            isFolder: true,
+            children: ['child1', 'child2', 'child3'],
+            data: 'Root item',
+            canRename: true,
+        },
+        child1: {
+            index: 'child1',
+            canMove: true,
+            isFolder: true,
+            children: [],
+            data: 'Child item 1',
+            canRename: true,
+        },
+        child2: {
+            index: 'child2',
+            canMove: true,
+            isFolder: true,
+            children: [
+                'child2_1'
+            ],
+            data: 'Child item 2',
+            canRename: true,
+        },
+        child2_1: {
+            index: 'child2_1',
+            canMove: true,
+            isFolder: false,
+            children: [],
+            data: 'Child item 2_1',
+            canRename: true,
+        },
+        child3: {
+            index: 'child3',
+            canMove: true,
+            isFolder: true,
+            children: [],
+            data: 'Child item 3',
+            canRename: true,
+        },
+    };
+    const onDrop = (items, target) => {
+        console.log('dropEvent', items);
+        console.log('dropevent target', target);
+    };
+    return (
+        <>
+            <style>{`
+            .rct-tree-item-button{
+                color: #e8eaed;
+            }
+        :root {
+          --rct-color-tree-bg: #202020;
+          --rct-color-tree-focus-outline: #ffffff;
+          --rct-color-focustree-item-selected-bg: #333;
+          --rct-color-focustree-item-focused-border: #fff;
+          --rct-color-focustree-item-draggingover-bg: #333;
+          --rct-color-focustree-item-draggingover-color: inherit;
+          --rct-color-search-highlight-bg: #333;
+          --rct-color-drag-between-line-bg: #333;
+          --rct-color-arrow: #e8eaed;
+          --rct-item-height: 30px;
+          --rct-search-text: #e8eaed
+          --rct-arrow-size: 10px;
+          --rct-focus-outline: #e8eaed;
+          --rct-color-focustree-item-selected-text: #e8eaed;
+          --rct-color-focustree-item-hover-text: #333
+        }
+      `}</style>
+            <UncontrolledTreeEnvironment
+                dataProvider={new StaticTreeDataProvider(items, (item, data) => ({ ...item, data }))}
+                getItemTitle={item => item.data}
+                viewState={{}}
+                canDragAndDrop={true}
+                canDropOnFolder={true}
+                canReorderItems={true}
+                onDrop={(items, target) => { onDrop(items, target) }}
+            >
+                <Tree treeId="tree-1" rootItem="root" treeLabel="Tree Example" />
+            </UncontrolledTreeEnvironment>
+        </>
+    );
+}
--- src/layout/LeftSidebar/index.js
+++ src/layout/LeftSidebar/index.js
@@ -1,10 +1,137 @@
 import React from "react";
+import AppBar from '@mui/material/AppBar';
+import Stack from '@mui/material/Stack';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import setab_logo from '../../assets/setab.jpg';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import MenuItem from '@mui/material/MenuItem';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import Settings from '@mui/icons-material/Settings';
+import Logout from '@mui/icons-material/Logout';
+import Divider from '@mui/material/Divider';
+import Box from '@mui/material/Box';
+import AddIcon from '@mui/icons-material/Add';
+import Grid from '@mui/material/Grid';
+import MenuList from './MenuList';
+
+
+
 
 const LeftSidebar = ({ cssLeftSidebar, styleLeftSidebar }) => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
-        <div className={cssLeftSidebar} style={styleLeftSidebar}>
-            LeftSidebar
-        </div>
+        <>
+
+            <div className={cssLeftSidebar} style={styleLeftSidebar}>
+                <AppBar position="static" color="inherit" style={{ backgroundColor: '#202020' }}>
+                    <Toolbar variant="dense">
+                        <Typography variant="h1" color="white" component="div">
+
+                            <Box sx={{ flexGrow: 1 }}>
+                                <Grid container >
+                                    <Grid item xs={10}>
+                                        < img src={setab_logo} alt="setab_logo" width="100%" />
+                                    </Grid>
+                                    <Grid item xs={2} style={{ lineHeight: '60px' }}>
+
+                                        <IconButton
+                                            onClick={handleClick}
+                                            size="large"
+                                            sx={{ ml: 2 }}
+                                            aria-controls={open ? 'account-menu' : undefined}
+                                            aria-haspopup="true"
+                                            aria-expanded={open ? 'true' : undefined}
+                                        >
+                                            <Avatar sx={{ width: 32, height: 32 }}><AddIcon></AddIcon></Avatar>
+                                        </IconButton>
+                                        <Menu
+                                            anchorEl={anchorEl}
+                                            id="account-menu"
+                                            open={open}
+                                            onClose={handleClose}
+                                            onClick={handleClose}
+                                            PaperProps={{
+                                                elevation: 0,
+                                                sx: {
+                                                    overflow: 'visible',
+                                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                    mt: 1.5,
+                                                    '& .MuiAvatar-root': {
+                                                        width: 32,
+                                                        height: 32,
+                                                        ml: -0.5,
+                                                        mr: 1,
+                                                    },
+                                                    '&:before': {
+                                                        content: '""',
+                                                        display: 'block',
+                                                        position: 'absolute',
+                                                        top: 0,
+                                                        right: 14,
+                                                        width: 10,
+                                                        height: 10,
+                                                        bgcolor: 'background.paper',
+                                                        transform: 'translateY(-50%) rotate(45deg)',
+                                                        zIndex: 0,
+                                                    },
+                                                },
+                                            }}
+                                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                        >
+                                            <MenuItem onClick={handleClose}>
+                                                <Avatar /> Profile
+                                            </MenuItem>
+                                            <MenuItem onClick={handleClose}>
+                                                <Avatar /> My account
+                                            </MenuItem>
+                                            <Divider />
+                                            <MenuItem onClick={handleClose}>
+                                                <ListItemIcon>
+                                                    <PersonAdd fontSize="small" />
+                                                </ListItemIcon>
+                                                Add another account
+                                            </MenuItem>
+                                            <MenuItem onClick={handleClose}>
+                                                <ListItemIcon>
+                                                    <Settings fontSize="small" />
+                                                </ListItemIcon>
+                                                Settings
+                                            </MenuItem>
+                                            <MenuItem onClick={handleClose}>
+                                                <ListItemIcon>
+                                                    <Logout fontSize="small" />
+                                                </ListItemIcon>
+                                                Logout
+                                            </MenuItem>
+                                        </Menu>
+                                    </Grid>
+
+                                </Grid>
+
+                            </Box>
+
+
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <Divider light />
+                <MenuList></MenuList>
+            </div>
+        </>
     );
 };
 
